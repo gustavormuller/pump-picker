9d9d2c36f236cf326a718886a5bfdb2080e240e9
Gustavo Muller <gusrmuller@gmail.com>
2023-04-06 15:57:06 -0300

    add responsiviness

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
 const options = {
     method: 'GET',
     headers: {
